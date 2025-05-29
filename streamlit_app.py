33d8efe9195db52f14f80023e587afa8cb525d4c
Bootan <htsu159@gmail.com>
2025-05-29 15:18:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import requests
 
 # Set the app title 
-st.title('My First Streamlit App !!') 
+st.title('ZAMANI AYAM GUNTING !!') 
 
 # Add a welcome message 
-st.write('Welcome to my Streamlit app!') 
+st.write('MAKAN TENGAH HARI!') 
 
 # Create a text input 
 widgetuser_input = st.text_input('Enter a custom message:', 'Hello, Streamlit!') 
